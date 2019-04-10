d849efe2533ccf6cd46c7673179531e73d0f6b8d
Andrzej Klajnert <github@aklajnert.pl>
2019-04-10 16:32:21 +0200

    Reformat, add more env variables.

--- docker-settings.py
+++ docker-settings.py
@@ -11,67 +11,62 @@ def env(variable, default=None):
     return os.environ.get(variable, default)
 
 
-DOMAIN = env('RTD_DOMAIN', 'localhost')
+DOMAIN = env("RTD_DOMAIN", "localhost")
+
 
 class DockerSettings(CommunityBaseSettings):
     PRODUCTION_DOMAIN = DOMAIN
     PUBLIC_DOMAIN = DOMAIN
-    WEBSOCKET_HOST = f'{DOMAIN}:8088'
+    WEBSOCKET_HOST = f"{DOMAIN}:8088"
 
-    DEBUG = env('RTD_DEBUG', False)
+    DEBUG = env("RTD_DEBUG", False)
     SERVE_DOCS = [constants.PUBLIC, constants.PRIVATE]
 
-    PYTHON_MEDIA = True # this is temporary until I'll figure out how to force uwsgi to serve media
+    PYTHON_MEDIA = True  # this is temporary until I'll figure out how to force uwsgi to serve media
 
     @property
     def DATABASES(self):  # noqa
         return {
-            'default': {
-                'ENGINE': 'django.db.backends.postgresql',
-                'NAME': env('RTD_DB_NAME', 'rtd'),
-                'USER': env('RTD_DB_USER', 'rtd-user'),
-                'PASSWORD': env('RTD_DB_PASS', 'rtd-password'),
-                'HOST': env('RTD_DB_HOST', 'db'),
+            "default": {
+                "ENGINE": "django.db.backends.postgresql",
+                "NAME": env("RTD_DB_NAME", "rtd"),
+                "USER": env("RTD_DB_USER", "rtd-user"),
+                "PASSWORD": env("RTD_DB_PASS", "rtd-password"),
+                "HOST": env("RTD_DB_HOST", "db"),
             }
         }
 
     DONT_HIT_DB = False
 
-    ACCOUNT_EMAIL_VERIFICATION = 'none'
+    ACCOUNT_EMAIL_VERIFICATION = "none"
     SESSION_COOKIE_DOMAIN = None
-    CACHE_BACKEND = 'dummy://'
+    CACHE_BACKEND = "dummy://"
 
-    SLUMBER_USERNAME = 'slumber'
-    SLUMBER_PASSWORD = '<slumber-password>'
-    SLUMBER_API_HOST = 'http://127.0.0.1:8000'
-    PUBLIC_API_URL = 'http://127.0.0.1:8000'
+    SLUMBER_USERNAME = env("RTD_SLUMBER_USER", "slumber")
+    SLUMBER_PASSWORD = env("RTD_SLUMBER_PASS", "<slumber-password>")
+    SLUMBER_API_HOST = "http://127.0.0.1:8000"
+    PUBLIC_API_URL = "http://127.0.0.1:8000"
 
     REDIS_HOST = env("RTD_REDIS_HOST", "redis")
     REDIS_PORT = env("RTD_REDIS_PORT", "6379")
 
-    BROKER_URL = f'redis://{REDIS_HOST}:{REDIS_PORT}/0'
-    CELERY_RESULT_BACKEND = f'redis://{REDIS_HOST}:{REDIS_PORT}/0'
-    CELERY_RESULT_SERIALIZER = 'json'
+    BROKER_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/0"
+    CELERY_RESULT_BACKEND = f"redis://{REDIS_HOST}:{REDIS_PORT}/0"
+    CELERY_RESULT_SERIALIZER = "json"
     CELERY_ALWAYS_EAGER = True
     CELERY_TASK_IGNORE_RESULT = False
 
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    FILE_SYNCER = 'readthedocs.builds.syncers.LocalSyncer'
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    FILE_SYNCER = "readthedocs.builds.syncers.LocalSyncer"
 
-    CORS_ORIGIN_WHITELIST = (
-        '0.0.0.0:8000',
-    )
+    CORS_ORIGIN_WHITELIST = ("0.0.0.0:8000",)
 
     ELASTIC_HOST = env("RTD_ELASTIC_HOST", "elasticsearch")
     ELASTIC_PORT = env("RTD_ELASTIC_PORT", "9200")
 
     ELASTICSEARCH_DSL_AUTOSYNC = False
-    ES_HOSTS = [f'{ELASTIC_HOST}:{ELASTIC_PORT}']
-    ELASTICSEARCH_DSL = {
-        'default': {
-            'hosts': f'{ELASTIC_HOST}:{ELASTIC_PORT}'
-        },
-    }
+    ES_HOSTS = [f"{ELASTIC_HOST}:{ELASTIC_PORT}"]
+    ELASTICSEARCH_DSL = {"default": {"hosts": f"{ELASTIC_HOST}:{ELASTIC_PORT}"}}
 
 
 DockerSettings.load_settings(__name__)
