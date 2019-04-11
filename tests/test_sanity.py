b8131249aca61039bb224bdcafa4e4762e183367
Andrzej Klajnert <github@aklajnert.pl>
2019-04-11 16:38:17 +0200

    Slightly extended test.

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -1,9 +1,13 @@
 from requests_html import HTMLSession
 
+DESIRED_RTD_VERSION = '3.4.1'
 
 def test_sanity(app):
     """Check if the basic functionaity works correctly."""
     port, username, password = app
     session = HTMLSession()
     response = session.get('http://localhost:{}'.format(port))
+
+    assert len(response.html.find('a', containing=DESIRED_RTD_VERSION)) == 1
+
     assert response.status_code == 200
