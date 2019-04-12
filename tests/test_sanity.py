7bbcb8605e5d1b09a24181fcd29c5cd296f13ec9
Andrzej Klajnert <github@aklajnert.pl>
2019-04-12 16:24:50 +0200

    Changed test to use selenium.

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -1,13 +1,45 @@
-from requests_html import HTMLSession
+from pathlib import Path
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 DESIRED_RTD_VERSION = '3.4.1'
 
+
+class Driver:
+    """Context manager to handle chrome driver."""
+
+    def __init__(self):
+        self.driver = None
+
+    def __enter__(self):
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+
+        self.driver = webdriver.Chrome(Path(__file__).parent / 'chromedriver', options=chrome_options)
+        return self.driver
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.driver.close()
+
+
 def test_sanity(app):
-    """Check if the basic functionaity works correctly."""
+    """Check if the basic functionality works correctly."""
     port, username, password = app
-    session = HTMLSession()
-    response = session.get('http://localhost:{}'.format(port))
 
-    assert len(response.html.find('a', containing=DESIRED_RTD_VERSION)) == 1
+    with Driver() as driver:
+        driver.get('http://localhost:{}'.format(port))
 
-    assert response.status_code == 200
+        version = driver.find_element_by_xpath("//a[@href='http://docs.readthedocs.io/page/changelog.html']")
+        assert version.text == DESIRED_RTD_VERSION
+
+        _log_in(driver, password, username)
+
+        assert driver.title == 'Project Dashboard | Read the Docs'
+
+
+def _log_in(driver, password, username):
+    driver.find_element_by_xpath("//a[contains(text(),'Log in')]").click()
+    driver.find_element_by_id('id_login').send_keys(username)
+    driver.find_element_by_id('id_password').send_keys(password)
+    driver.find_element_by_xpath("//button[contains(.,'Sign In')]").click()
