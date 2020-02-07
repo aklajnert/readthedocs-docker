4bce38dcc09ec0cd43254f1462fbfecfc779bd70
Andrzej Klajnert <andrzejk@cadence.com>
2020-02-07 08:54:44 +0100

    Update to 3.11.6

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-DESIRED_RTD_VERSION = "3.11.3"
+DESIRED_RTD_VERSION = "3.11.6"
 SHOW_BROWSER = os.environ.get("SHOW_BROWSER", False)
 
 
